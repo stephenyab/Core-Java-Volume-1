be2bfd34643ab9b4e9e6059bd684ec702752b4bb
stephen.yab <stephen.yab@gmail.com>
2019-10-23 22:16:00 +0800

    for loop,BigInteger and BigDecimal

---
+++ chapter_3/BigInteger_5.java
@@ -0,0 +1,23 @@
+package Core_Java_Volume_1.chapter_3;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigInteger_5 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        System.out.print("How many numbers do you need to draw? ");
+        int k=in.nextInt();
+
+        System.out.print("What is the highest number you can draw? ");
+        int n=in.nextInt();
+
+        BigInteger lotteryOdds=BigInteger.valueOf(1);
+
+        for(int i=1;i<=k;i++)
+            lotteryOdds=lotteryOdds.multiply(BigInteger.valueOf(n-i+1)).divide(BigInteger.valueOf(i));
+
+        System.out.print("Your odds are 1 in "+lotteryOdds+". Good luck!");
+    }
+}
