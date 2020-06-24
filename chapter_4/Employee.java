2404e3ef795582d333822bb1f7a64e098b96831d
stephenyab <stephen.yab@gmail.com>
2020-06-24 18:46:14 +0800

    create clas Employee

---
+++ chapter_4/Employee.java
@@ -0,0 +1,47 @@
+package Core_Java_Volume_1.chapter_4;
+
+import java.time.LocalDate;
+
+/**
+ * @ClassName Employee
+ * @Description
+ * @Author 26232
+ * @Date 2020/6/24 18:43
+ * @Version 1.0
+ */
+public class Employee {
+
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String n, double s, int year, int month, int day) {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void setHireDay(LocalDate hireDay) {
+        this.hireDay = hireDay;
+    }
+}
