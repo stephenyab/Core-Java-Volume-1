02523e11aa96d9a127ff023b451d6972325599ee
stephenyab <stephen.yab@gmail.com>
2020-06-24 18:29:45 +0800

    fix something

--- chapter_4/CalendarTest_1.java
+++ chapter_4/CalendarTest_1.java
@@ -14,19 +14,23 @@ public class CalendarTest_1 {
         int value = weekday.getValue();
 
         System.out.println("Mon Tue Wed Thu Fri Sat Sun");
-        for (int i = 1; i < value; i++)
+        for (int i = 1; i < value; i++) {
             System.out.print("    ");
+        }
         while (date.getMonthValue() == month) {
             System.out.printf("%3d", date.getDayOfMonth());
-            if (date.getDayOfMonth() == today)
+            if (date.getDayOfMonth() == today) {
                 System.out.print("*");
-            else
+            } else {
                 System.out.print(" ");
+            }
             date = date.plusDays(1);
-            if (date.getDayOfWeek().getValue() == 1)
+            if (date.getDayOfWeek().getValue() == 1) {
                 System.out.println();
+            }
         }
-        if (date.getDayOfWeek().getValue() != 1)
+        if (date.getDayOfWeek().getValue() != 1) {
             System.out.println();
+        }
     }
 }
